386b05f3a8152c2c34e574c04408a719598595ee
Jeff Xiong <gigix1980@gmail.com>
2020-02-15 17:44:16 +0800

    fix build

---
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -0,0 +1,15 @@
+package com.jiker.keju;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+        /*TODO
+          args[0]为resources下的测试数据文件名。
+          读取测试数据文件作为你程序的输入参数，
+          并将结果保存到receipt变量中。
+         */
+        String testDataFile = args[0];
+        String receipt = new Receipt(testDataFile).bill();
+        System.out.println(receipt);
+    }
+}
--- src/main/java/com/jiker/keju/Taxi.java
+++
@@ -1,8 +0,0 @@
-package com.jiker.keju;
-
-public class Taxi {
-    public static void main(String[] args) {
-        Receipt receipt = new Receipt("src/main/resources/testData.txt");
-        System.out.println(receipt.bill());
-    }
-}
